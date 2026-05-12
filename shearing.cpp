778f4062b85e0b5a49b1accf0490bea60dd0ca5c
pretsolomi <pretsolomi@gmail.com>
2026-05-13 02:00:11 +0530

    Update shearing.cpp

--- shearing.cpp
+++ shearing.cpp
@@ -1,7 +1,8 @@
-//9e implement code to make line shearing  in x and y
+// Implement line shearing in X and Y
 #include <graphics.h>
 #include <iostream>
-#include<conio.h>
+#include <conio.h>
+
 using namespace std;
 
 int main()
@@ -9,44 +10,36 @@ int main()
     int x1, y1, x2, y2;
     int shx, shy;
 
-    cout << "Enter the endpoints of the line:\n";
+    cout << "Enter line coordinates: ";
     cin >> x1 >> y1 >> x2 >> y2;
 
-    cout << "Enter shear factor along X-axis: ";
+    cout << "Enter X-shear factor: ";
     cin >> shx;
 
-    initwindow(800, 600, "Line Shearing");
+    cout << "Enter Y-shear factor: ";
+    cin >> shy;
 
+    initwindow(1000, 1000, "Line Shearing");
+
+    // Original line
     setcolor(WHITE);
-    outtextxy(10, 10, "WHITE: Before Shearing");
+    outtextxy(10, 10, "Original Line");
     line(x1, y1, x2, y2);
 
-    setcolor(RED);
-    outtextxy(10, 30, "RED: After Shearing along X-axis");
-
-    // Apply shear (X-axis)
+    // X-axis shearing
     int new_x1 = x1 + shx * y1;
     int new_x2 = x2 + shx * y2;
 
-
+    setcolor(RED);
+    outtextxy(10, 30, "X-Sheared Line");
     line(new_x1, y1, new_x2, y2);
 
-    cout << "Enter shear factor along Y-axis: ";
-    cin >> shy;
-
-	char msg1[] = "WHITE: Before Shearing";
-	char msg2[] = "RED: After Shearing along X-axis";
-    setcolor(WHITE);
-    outtextxy(10, 10, msg1);
-    line(x1, y1, x2, y2);
-
-    setcolor(RED);
-    outtextxy(10, 30, msg2);
-
-    // Apply shear (Y-axis)
+    // Y-axis shearing
     int new_y1 = y1 + shy * x1;
     int new_y2 = y2 + shy * x2;
 
+    setcolor(GREEN);
+    outtextxy(10, 50, "Y-Sheared Line");
     line(x1, new_y1, x2, new_y2);
 
     getch();
